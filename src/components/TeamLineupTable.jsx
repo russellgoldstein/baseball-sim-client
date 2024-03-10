1c772c723c139259f8af6c1baa4a31c1baea43da
Russ Goldstein <russell.goldstein@gmail.com>
2024-03-10 06:41:52 -0400

    Fix

--- src/components/TeamLineupTable.jsx
+++ src/components/TeamLineupTable.jsx
@@ -14,7 +14,7 @@ export const TeamLineupTable = ({
 }) => {
   return (
     <div className='flex flex-container flex-col justify-start items-stretch space-y-6'>
-      test
+      <h2 className='text-2xl font-semibold text-center mb-4'>Lineup</h2>
       <TeamHittersLineupTable
         lineup={hittingLineup}
         setLineup={setHittingLineup}
